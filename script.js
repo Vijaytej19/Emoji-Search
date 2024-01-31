758126cddb32eea51e9554d8bd01e45f52ba9567
Vijaytej19 <vijayteja2000@gmail.com>
2024-01-31 15:55:04 +0530

    Added tags to searchFunctionality

--- script.js
+++ script.js
@@ -1,46 +1,44 @@
-fetch('emoji.json')
-.then(response => response.json())
-.then(data => {
+fetch("emoji.json")
+  .then((response) => response.json())
+  .then((data) => {
     // Process the data and display in UI
-    const emojiContainer = document.getElementById('emojiContainer');
+    const emojiContainer = document.getElementById("emojiContainer");
 
-    data.forEach(emojiData => {
-        const emojiDiv = document.createElement('div');
-        emojiDiv.classList.add('commonClass', 'emoji-item');
-        emojiDiv.innerHTML = `
+    data.forEach((emojiData) => {
+      const emojiDiv = document.createElement("div");
+      emojiDiv.classList.add("commonClass", "emoji-item");
+      emojiDiv.innerHTML = `
         <div class = "sinleEmojiContainer">
             <p class="emoji"> ${emojiData.emoji}</p>
             <p class="emojiDescription"> ${emojiData.aliases}</p>
+            <p class="emojiTags">${emojiData.tags}</p>
+        
         </div>
         
         `;
-        emojiContainer.appendChild(emojiDiv);   
+      emojiContainer.appendChild(emojiDiv);
     });
-    
-
-  
-
-
-})
-.catch(error => console.log('Error fetching data:', error));
+  })
+  .catch((error) => console.log("Error fetching data:", error));
 
 function searchFunction() {
-    const searchInput = document.getElementById('searchInput');
-    const searchValue = searchInput.value.toLowerCase();
-    const emojiItems = document.querySelectorAll('.emoji-item');
+  const searchInput = document.getElementById("searchInput");
+  const searchValue = searchInput.value.trim().toLowerCase();
+  const emojiItems = document.querySelectorAll(".emoji-item");
 
-    emojiItems.forEach(item => {
-        const emojiDescription = item.querySelector('.emojiDescription').textContent.toLowerCase();
-        if (emojiDescription.includes(searchValue)) {
-            item.style.display = 'block';
-        } else {
-            item.style.display = 'none';
-        }
-    });
+  emojiItems.forEach((item) => {
+    const emojiDescription = item
+      .querySelector(".emojiDescription")
+      .textContent.toLowerCase();
+    const emojiTags = item
+      .querySelector(".emojiTags")
+      .textContent.toLowerCase();
+
+    const shouldDisplay =
+      emojiDescription.includes(searchValue) || emojiTags.includes(searchValue);
+    item.style.display = shouldDisplay || searchValue === "" ? "block" : "none";
+  });
 }
-const searchButton = document.getElementById('searchButton');
-searchButton.onclick = searchFunction
 
-
-
-
+const searchButton = document.getElementById("searchButton");
+searchButton.onclick = searchFunction;
